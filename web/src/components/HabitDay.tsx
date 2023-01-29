0fbb4da26bde8c7b57cd8c240102267493f53492
Edu Leite <nisaneduardo@gmail.com>
2023-01-29 18:22:24 -0300

    finalizando rotas back end, continuacao front end

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+import "./Habit.css";
+
+// interface HabitDayProps {
+//   completed: number;
+// }
+
+export const HabitDay = () => {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-spacing-2 border-zinc-800 rounded-lg"></div>
+  );
+};
