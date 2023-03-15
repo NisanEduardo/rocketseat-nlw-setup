8bc005edf145fff8066d9b02e0e2e000d67badb7
Edu Leite <nisaneduardo@gmail.com>
2023-03-15 13:19:10 -0300

    finalização do projeto com adição de transições e detalhes dos habitos

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -5,18 +5,21 @@ import { ProgressBar } from "./ProgressBar";
 
 import dayjs from "dayjs";
 import { HabitsList } from "./HabitsList";
+import { useState } from "react";
 
 interface HabitDayProps {
   date: Date;
-  completed?: number;
+  defaultCompleted?: number;
   amount?: number;
 }
 
 export const HabitDay = ({
-  completed = 0,
+  defaultCompleted = 0,
   amount = 0,
   date,
 }: HabitDayProps) => {
+  const [completed, setCompleted] = useState(defaultCompleted);
+
   const completedPercentage =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
@@ -24,24 +27,27 @@ export const HabitDay = ({
   const dayOfWeek = dayjs(date).format("dddd");
 
   function handleCompletedChanged(completed: number) {
-    console.log(completed);
+    setCompleted(completed);
   }
 
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10 border-spacing-2 rounded-lg", {
-          "bg-zinc-900 border-zinc-800": completedPercentage === 0,
-          "bg-violet-900 border-violet-700":
-            completedPercentage > 0 && completedPercentage < 20,
-          "bg-violet-800 border-violet-600":
-            completedPercentage >= 20 && completedPercentage < 40,
-          "bg-violet-700 border-violet-500":
-            completedPercentage >= 40 && completedPercentage < 60,
-          "bg-violet-600 border-violet-500":
-            completedPercentage >= 60 && completedPercentage < 80,
-          "bg-violet-500 border-violet-400": completedPercentage >= 80,
-        })}
+        className={clsx(
+          "w-10 h-10 border-spacing-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-background",
+          {
+            "bg-zinc-900 border-zinc-800": completedPercentage === 0,
+            "bg-violet-900 border-violet-700":
+              completedPercentage > 0 && completedPercentage < 20,
+            "bg-violet-800 border-violet-600":
+              completedPercentage >= 20 && completedPercentage < 40,
+            "bg-violet-700 border-violet-500":
+              completedPercentage >= 40 && completedPercentage < 60,
+            "bg-violet-600 border-violet-500":
+              completedPercentage >= 60 && completedPercentage < 80,
+            "bg-violet-500 border-violet-400": completedPercentage >= 80,
+          }
+        )}
       />
 
       <Popover.Portal>
