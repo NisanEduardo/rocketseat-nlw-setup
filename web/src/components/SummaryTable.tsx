718f7632da1ad8d7e0da4f06f34bfd2b04b8d9a9
Edu Leite <nisaneduardo@gmail.com>
2024-11-10 17:59:33 -0300

    feat: add static db url file , fix to showing remaining  days to end year

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -12,63 +12,63 @@ const minimumSummaryDatesSize = 18 * 7; // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 type Summary = Array<{
-  id: string;
-  date: string;
-  amount: number;
-  completed: number;
+	id: string;
+	date: string;
+	amount: number;
+	completed: number;
 }>;
 
 export function SummaryTable() {
-  const [summary, setSummary] = useState<Summary>([]);
+	const [summary, setSummary] = useState<Summary>([]);
 
-  useEffect(() => {
-    api.get("summary").then((response) => {
-      setSummary(response.data);
-    });
-  }, []);
+	useEffect(() => {
+		api.get("summary").then((response) => {
+			setSummary(response.data);
+		});
+	}, []);
 
-  return (
-    <div className="w-full flex">
-      <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay, index) => (
-          <div
-            key={`${weekDay}-${index}`}
-            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
-          >
-            {weekDay}
-          </div>
-        ))}
-      </div>
+	return (
+		<div className="w-full flex">
+			<div className="grid grid-rows-7 grid-flow-row gap-3">
+				{weekDays.map((weekDay, index) => (
+					<div
+						key={`${weekDay}-${index}`}
+						className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+					>
+						{weekDay}
+					</div>
+				))}
+			</div>
 
-      <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summary.length > 0 &&
-          summaryDates.map((date) => {
-            const dayInSummary = summary.find((day) => {
-              return dayjs(date).isSame(day.date, "day");
-            });
+			<div className="grid grid-rows-7 grid-flow-col gap-3">
+				{summary.length > 0 &&
+					summaryDates.map((date) => {
+						const dayInSummary = summary.find((day) => {
+							return dayjs(date).isSame(day.date, "day");
+						});
 
-            return (
-              <HabitDay
-                key={date.toString()}
-                date={date}
-                defaultCompleted={dayInSummary?.completed}
-                amount={dayInSummary?.amount}
-              />
-            );
-          })}
+						return (
+							<HabitDay
+								key={date.toString()}
+								date={date}
+								defaultCompleted={dayInSummary?.completed}
+								amount={dayInSummary?.amount}
+							/>
+						);
+					})}
 
-        {amountOfDaysToFill > 0 &&
-          Array.from({
-            length: amountOfDaysToFill,
-          }).map((_, index) => {
-            return (
-              <div
-                key={index}
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-              />
-            );
-          })}
-      </div>
-    </div>
-  );
+				{amountOfDaysToFill > 0 &&
+					Array.from({
+						length: amountOfDaysToFill,
+					}).map((_, index) => {
+						return (
+							<div
+								key={index}
+								className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+							/>
+						);
+					})}
+			</div>
+		</div>
+	);
 }
